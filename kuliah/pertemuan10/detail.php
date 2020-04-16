523ed75d324eb9c8b911dcb6073d60640398ea3e
YouuDan <wijdan.m.ridwanulloh@gmail.com>
2020-04-16 20:55:44 +0700

    change detail.php style

--- kuliah/pertemuan10/detail.php
+++ kuliah/pertemuan10/detail.php
@@ -18,20 +18,37 @@ $mahasiswa = query("SELECT * FROM mahasiswa WHERE id = $id");
 </head>
 
 <body>
-  <h3>Detail Mahasiswa</h3>
-  <ul>
-    <li><img src="img/<?= $mahasiswa['gambar']; ?>" alt=""></li>
-    <li>NRP : <?= $mahasiswa['nrp']; ?></li>
-    <li>Nama : <?= $mahasiswa['nama']; ?></li>
-    <li>Email : <?= $mahasiswa['email']; ?></li>
-    <li>Jurusan : <?= $mahasiswa['jurusan']; ?></li>
-    <li><a href="">ubah</a> | <a href="">hapus</a`>
-    </li>
-    <li><a href="latihan3.php">kembali</a></li>
-  </ul>
-  <script>
-    import 'bootstrap/dist/css/bootstrap.min.css';
-  </script>
+  <div class="container mt-5">
+    <div class="row justify-content-md-center">
+      <div class="col-md-8">
+        <div class="card d-flex justify-content-center">
+          <div class="card-header">
+            <h3>Detail Mahasiswa</h3>
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md">
+                <img src="img/<?= $mahasiswa['gambar']; ?>" alt="">
+              </div>
+              <div class="col-md">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">NRP : <?= $mahasiswa['nrp']; ?></li>
+                  <li class="list-group-item">Nama : <?= $mahasiswa['nama']; ?></li>
+                  <li class="list-group-item">Email : <?= $mahasiswa['email']; ?></li>
+                  <li class="list-group-item">Jurusan : <?= $mahasiswa['jurusan']; ?></li>
+                  <li class="list-group-item">
+                    <a class="btn btn-primary btn-block" href="">ubah</a>
+                    <a class="btn btn-danger btn-block" href="">hapus</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <a class="btn btn-secondary mt-3" href="latihan3.php">&laquo; kembali</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
