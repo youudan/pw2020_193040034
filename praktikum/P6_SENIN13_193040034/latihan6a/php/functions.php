018cda27cf15cb5c2af768b455b31907d9f0e628
YouuDan <wijdan.m.ridwanulloh@gmail.com>
2020-04-20 15:41:35 +0700

    fix db name & password

--- praktikum/P6_SENIN13_193040034/latihan6a/php/functions.php
+++ praktikum/P6_SENIN13_193040034/latihan6a/php/functions.php
@@ -1,24 +1,26 @@
 <?php
 
-  // Koneksi database
-  function koneksi(){
-    $host     = 'localhost';
-    $username = 'root';
-    $password = 'root';
-    $db       = 'tubes_193040034';
+// Koneksi database
+function koneksi()
+{
+  $host     = 'localhost';
+  $username = 'root';
+  $password = '';
+  $db       = 'pw_193040034';
 
-    $conn = mysqli_connect($host, $username, $password) or die("koneksi DB gagal");
-    mysqli_select_db($conn, $db) or die("database tidak ditemukan");
-    return $conn;
+  $conn = mysqli_connect($host, $username, $password) or die("koneksi DB gagal");
+  mysqli_select_db($conn, $db) or die("database tidak ditemukan");
+  return $conn;
+}
+// Query
+function query($sql)
+{
+  $result = mysqli_query(koneksi(), "$sql");
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
   }
-  // Query
-  function query($sql){
-    $result = mysqli_query(koneksi(), "$sql");   
 
-    $rows = [];
-    while ($row = mysqli_fetch_assoc($result)) {
-      $rows[] = $row;
-    }
-
-    return $rows;
-  }
+  return $rows;
+}
