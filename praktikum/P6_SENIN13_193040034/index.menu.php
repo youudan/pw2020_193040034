1df6ba428718f79a3c0c449a88c3672193c26760
YouuDan <wijdan.m.ridwanulloh@gmail.com>
2020-05-04 11:35:42 +0700

    fix some errors

--- praktikum/P6_SENIN13_193040034/index.menu.php
+++ praktikum/P6_SENIN13_193040034/index.menu.php
@@ -19,7 +19,7 @@
       <hr class="my-4 bg-light">
 
     </div>
-    <a href="latihan6a/index.php" target="contents" class="btn btn-gits btn-block">LATIHAN 6A</a>
+    <a href="latihan6a/index.php" target="contents" class="btn btn-warning btn-block">LATIHAN 6A</a>
     <a href="latihan6b/index.php" target="contents" class="btn btn-warning btn-block">LATIHAN 6B</a>
     <a href="latihan6c/index.php" target="contents" class="btn btn-warning btn-block">LATIHAN 6C</a>
     <a href="latihan6d/index.php" target="contents" class="btn btn-warning btn-block">LATIHAN 6D</a>
