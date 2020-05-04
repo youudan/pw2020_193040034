48883371d1edf8e6d80643cc8baaf6fcddc22664
YouuDan <wijdan.m.ridwanulloh@gmail.com>
2020-05-04 11:40:22 +0700

    fix some errors for hosting

---
+++ praktikum/P7_SENIN13_193040034/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="latihan7a/assets/css/style.css">
+  <title>193040034</title>
+</head>
+
+<body>
+  <a href="latihan7a/index">latihan7a</a>
+  <a href="latihan7b/index">latihan7b</a>
+  <a href="latihan7c/index">latihan7c</a>
+</body>
+
+</html>
