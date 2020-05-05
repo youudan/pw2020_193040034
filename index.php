b823c90c683762aad82c89c8fd4f8cdc6a13e3e1
YouuDan <wijdan.m.ridwanulloh@gmail.com>
2020-05-05 15:43:01 +0700

    add tubes

--- index.php
+++ index.php
@@ -193,7 +193,7 @@
         <div class="column">
           <h3 class="title title-profile">Tugas Besar</h3>
           <p>Tekan tombol dibawah untuk menuju <strong>Tugas Besar</strong>.</p>
-          <a class="button is-primary" href="tubes/">
+          <a class="button is-primary" href="tubes/" target="_blank">
             <strong>Tugas Besar</strong>
           </a>
         </div>
